2848d43ecce28a85db0cde50be71dcd4375ef6cd
jagansahu <37058620+jagansahu@users.noreply.github.com>
2020-03-17 03:14:10 -0400

    Updated

---
+++ test.py
@@ -0,0 +1,21 @@
+import discord
+import random
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+  print("Bot iss ready.")
+
+@client.command()
+async def ping(ctx):
+  await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+@client.command(aliases=["8ball"])
+async def _8ball(ctx, *, question):
+  responses = ["Yes", "No"]
+
+  await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
+
+client.run("NjY3MjI5MDM3NzE3ODgwODMy.XiHYYA.NHuVk-L1MuVRhfDracTuQPR_a_s")
